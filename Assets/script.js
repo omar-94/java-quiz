67c77198b7ff4422aa6f11ae8786b320ed9b5999
onava <enava23@yahoo.com>
2021-05-20 01:00:21 -0400

    added basic html / defined elements to variables in java

---
+++ Assets/script.js
@@ -0,0 +1,16 @@
+var quizContainer = document.getElementById('quiz');
+var resultContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+function startQuiz() {
+
+}
+
+function showResults() {
+
+}
+
+// DISPLAY QUIZ
+startQuiz();
+
+// WHEN NEXT QUESTION IS CLICKED... NEXT QUESTION IS SHOWN
