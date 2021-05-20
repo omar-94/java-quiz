e4c7e5cdf21d071bfa5ca949235b19ba99b2131c
onava <enava23@yahoo.com>
2021-05-20 01:28:50 -0400

    added questionns to var questions in java

--- Assets/script.js
+++ Assets/script.js
@@ -2,11 +2,67 @@ var quizContainer = document.getElementById('quiz');
 var resultContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
 
+// QUESTIONS TO BE ASKED
+var questions = [
+	{
+		question: "What are variables used for in JavaScript Programs?",
+		answers: {
+			a: "Storing numbers, dates, or other values", 
+			b: "Varying randomly",
+			c: "Causing high-school algebra flashbacks",
+			d: "None of the above",
+		},    
+		correctAnswer: "a"
+	},
+	{
+		question: "Inside which HTML element do we put the JavaScript?",
+		answers: {
+			a: "<js>", 
+			b: "<scripting>",
+			c: "<script>",
+			d: "<javascript",
+		},    
+		correctAnswer: "c"
+	},
+	{
+		question: "How can you get the total number of arguments passed to a function?",
+		answers: {
+			a: "Using args.length property", 
+			b: "Using arguments.length property",
+			c: "Both of the above",
+			d: "None of the above",
+		},    
+		correctAnswer: "b"
+	},
+	{
+		question: "Which built-in method returns the string representation of the number's value?",
+		answers: {
+			a: "toValue()", 
+			b: "toNumber()",
+			c: "toString()",
+			d: "None of the above",
+		},    
+		correctAnswer: "c"
+	},
+	{
+		question: "Which of the following function of Array object removes the last element from an array and returns that element?",
+		answers: {
+			a: "pop()", 
+			b: "push()",
+			c: "join()",
+			d: "map()",
+		},    
+		correctAnswer: "a"
+	},
+]
+
+
+// FUNCTIONS TO BE RUNNED 
 function startQuiz() {
 
 }
 
-function showResults() {
+function nextQuestion() {
 
 }
 
@@ -14,3 +70,5 @@ function showResults() {
 startQuiz();
 
 // WHEN NEXT QUESTION IS CLICKED... NEXT QUESTION IS SHOWN
+nextQuestion();
+
